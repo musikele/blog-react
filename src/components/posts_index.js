3d6eac2c9553a9d844625f642da7006259e6d853
Michele Nasti <michele@bemind.me>
2018-01-24 17:24:48 +0100

    log posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,40 @@
 import React, {Component } from 'react';
+import  { connect } from 'react-redux';
+import _ from 'lodash';
 
-export default class PostsIndex extends Componenet {
+import { fetchPosts } from '../actions';
+
+
+class PostsIndex extends Component {
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
-      <div>PostsIndex</div>
+      <div>
+        <h3>Post Index</h3>
+        <ul className="list-group">
+          { this.renderPosts() }
+        </ul>
+      </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
 import ReduxPromise from 'redux-promise';
+
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
@@ -14,9 +15,9 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostIndex} />
-        <Route path="/posts/:id" component={PostsShow} />
-        <Route path="/posts/new" component={PostsNew} />
+        <Route path="/" component={PostsIndex} />
+        {/* <Route path="/posts/:id" component={PostsShow} />
+        <Route path="/posts/new" component={PostsNew} /> */}
       </div>
     </BrowserRouter>
   </Provider>
