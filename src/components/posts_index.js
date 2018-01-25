9788913b3475b948c5738678f1d5aa7f5c2ce2d3
Michele Nasti <michele@bemind.me>
2018-01-25 16:23:52 +0100

    che burdell react

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, {Component } from 'react';
 import  { connect } from 'react-redux';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 import { fetchPosts } from '../actions';
 
@@ -24,7 +25,13 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a post
+          </Link>
+        </div>
         <h3>Post Index</h3>
+        <br />
         <ul className="list-group">
           { this.renderPosts() }
         </ul>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import reducers from './reducers';
 import ReduxPromise from 'redux-promise';
 
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
@@ -15,9 +16,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
-        {/* <Route path="/posts/:id" component={PostsShow} />
-        <Route path="/posts/new" component={PostsNew} /> */}
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+          {/* <Route path="/posts/:id" component={PostsShow} /> */}
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
